1b5c375589ed1945b8e652bb8c66c5ed9186908c
joinr <vangelder@tantalus.net>
2017-09-14 19:02:26 -0500

    Added dark nimbus as default, other plaf too zealous in forcing thingson EDT!

--- src/clojure/nightcode/window.clj
+++ src/clojure/nightcode/window.clj
@@ -13,11 +13,33 @@
            [org.pushingpixels.substance.api SubstanceLookAndFeel]
            [org.pushingpixels.substance.api.skin GraphiteSkin]))
 
+;;https://stackoverflow.com/questions/36128291/how-to-make-a-swing-application-have-dark-nimbus-theme-netbeans
+(defn set-nimbus! []
+  (do  (javax.swing.UIManager/put "control", (java.awt.Color.  128, 128, 128) );
+       (javax.swing.UIManager/put "info", (java.awt.Color. 128,128,128) );
+       (javax.swing.UIManager/put "nimbusBase", (java.awt.Color.  18, 30, 49) );
+       (javax.swing.UIManager/put "nimbusAlertYellow", (java.awt.Color.  248, 187, 0) );
+       (javax.swing.UIManager/put "nimbusDisabledText", (java.awt.Color.  128, 128, 128) );
+       (javax.swing.UIManager/put "nimbusFocus", (java.awt.Color. 115,164,209) );
+       (javax.swing.UIManager/put "nimbusGreen", (java.awt.Color. 176,179,50) );
+       (javax.swing.UIManager/put "nimbusInfoBlue", (java.awt.Color.  66, 139, 221) );
+       (javax.swing.UIManager/put "nimbusLightBackground", (java.awt.Color.  18, 30, 49) );
+       (javax.swing.UIManager/put "nimbusOrange", (java.awt.Color. 191,98,4) );
+       (javax.swing.UIManager/put "nimbusRed", (java.awt.Color. 169,46,34) );
+       (javax.swing.UIManager/put "nimbusSelectedText", (java.awt.Color.  255, 255, 255) );
+       (javax.swing.UIManager/put "nimbusSelectionBackground", (java.awt.Color.  104, 93, 156) );
+       (javax.swing.UIManager/put "text", (java.awt.Color.  230, 230, 230))
+     (doseq [info (javax.swing.UIManager/getInstalledLookAndFeels)]
+       (if (= "Nimbus" (.getName info))
+         (javax.swing.UIManager/setLookAndFeel (.getClassName info))))))
+
+
 (defn set-theme!
   "Sets the theme based on the command line arguments."
   [args]
   (s/native!)
-  (let [{:keys [shade skin-object theme-resource]} args]
+  (set-nimbus!)
+  #_(let [{:keys [shade skin-object theme-resource]} args]
     (when theme-resource (reset! ui/theme-resource theme-resource))
     (SubstanceLookAndFeel/setSkin (or skin-object (GraphiteSkin.)))))
 
